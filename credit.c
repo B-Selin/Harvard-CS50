ad95f8a6ef9f4a2daa91b6f3169b1af3b8d5bfce
flyingarburator <76648785+flyingarburator@users.noreply.github.com>
2021-12-25 16:10:55 +0300

    int to str now works fine

--- credit.c
+++ credit.c
@@ -3,21 +3,27 @@
 //MasterCard, or Visa card number, per the definitions of each’s format herein.
 //So that we can automate some tests of your code, we ask that
 //your program’s last line of output be AMEX\n or MASTERCARD\n or VISA\n or INVALID\n
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
 
 
 int main(void)
 {
-// Ask for credit card number, not sure if I should check the lenght or the sum first.
+// Ask for credit card number, length shooul be between 13-16 char
+    int l;
+    char myCard[17];
 	long i;
     do
     {
-	    i = get_long("Number: ");
-	    char cardNumber[17];
-	    sprintf(cardNumber, "%ld", i);
+        i = get_long("Number: ");
+        sprintf(myCard, "%ld", i);
+        l = sprintf(myCard, "%ld", i);
     }
-    while (12 >  i || i > 17);
+    while (12 >  l || l > 17);
 
-    printf("%f\n", cardNumber);
 
 
 //calculate checksum
