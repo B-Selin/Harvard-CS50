5ee42eafa6ff49a6dbfd5a1269cb916c0c80c5e6
flyingarburator <76648785+flyingarburator@users.noreply.github.com>
2022-01-16 13:15:06 +0300

    Notes

--- substitution.c
+++ substitution.c
@@ -26,13 +26,14 @@ int main(int argc, string argv[])
 
         if (n == 26) // Now have to check if all characters exist only once, if not then break? I will define the whole alphabet as a string maybe I will use that
             // this is not working and I'm tired. I came up with the idea summing the ascii values of both alphabet and arg, and see if they match. But first sleep I really dont know why I cant use string sort, it would fix all my problems.
-            //Couldnt sleep :p Ok when I checked the walkthroughs, I didnt understand how people solve it, so I read a billions code. Sortstring seemed like a very neat solution but it is not working in cs50 libraries? So I watched a billion more things on how to check duplicity. and now I understand how it is working
+            //Couldnt sleep :p Ok when I checked the walkthroughs, I didnt understand how people solve it, so I read a billions code. But I really dont understand how that code worked, so my own idea is sortstring and then stringcasecompare. 
+            //Sortstring seemed like a very neat solution but it is not working in cs50 libraries? So I watched a billion more things on how to check duplicity. and now I think I understand how it is working
             // for every letter in the key (variable is j) check if that letter occures one more time in the rest of the string (lets say k) check toupper(arg[1](j)) == toupper(arg[1][k])
         {
             for (int j = 0; j < n ; j++) // checking each element of string
             {
                 if (!isalpha(argv[1][j])) // if arg is alphabetic, check the lengt, has to be 26 letters
-                    //Failed at "handles invalid characters in key" :/ if numeric value is on the beginning, it reprompts user, but it doesnt to that when number is at the end. so I moved it from line 26 too here
+                    //Failed at "handles invalid characters in key" :/ if numeric value is on the beginning, it reprompts user, but it doesnt to that when number is at the end. so I moved it from line 26 to here
                 {
                     printf("Usage: ./substitution key\n");;
                     return 1;
@@ -51,7 +52,7 @@ int main(int argc, string argv[])
             int pL = strlen(plaintext);
             for (int i = 0; i < pL; i++)
             {
-                if (isupper(plaintext[i])) //change every upper character with upper case ofargv1
+                if (isupper(plaintext[i])) //change every upper character with upper case of argv1
                 {
                     printf("%c", toupper(argv[1][plaintext[i] - 65]));
                 }
@@ -73,5 +74,5 @@ int main(int argc, string argv[])
         }
     }
 
-// ok now the "supposedly" errory keys give error, and correct key continues with plantext. I have to find a way to replace plaintext with corresponding letter
+// ok now the "supposedly" errory keys give error, and correct key continues with plaintext. I have to find a way to replace plaintext with corresponding letter
 }
